82e7ca114815b76ba5af72e8fe4482c836f4a5ce
JohnsHarrison <jshar15089@gmail.com>
2022-08-12 09:42:48 -0400

    currently playing component set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { getPlaying } from './Services/api';
+import CurrentlyPlaying from './Components/CurrentlyPlaying'
+// console.log(process.env.REACT_APP_API_KEY1)
 
+
+getPlaying()
+// console.log(getPlaying())
+// console.log(getPlaying2())
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <CurrentlyPlaying></CurrentlyPlaying>
+       
     </div>
   );
 }
---
+++ src/Components/CurrentlyPlaying.js
@@ -0,0 +1,42 @@
+import { useState,useEffect } from "react";
+import { getPlaying } from "../Services/api";
+
+function CurrentlyPlaying(){
+const [playing,setPlaying] = useState([])
+
+async function displayMovies(){
+    const movies = await (await getPlaying()).data.items
+    console.log(movies)
+    const mappedMovies = movies.map((result,index)=>{
+        return(
+                <div className= {index % 2 === 0 ? "MovieContainerjh" : "MovieContainer2jh" } key = {index}>
+                <img className="currentimgjh" src={"https://imdb-api.com/API/ResizeImage?apiKey=k_2zuvks88&size=1012x1500&url=" + result.image} alt=''></img>
+                <div className="Infojh">
+                <h1>{result.title}({result.contentRating})</h1>
+                <h4>Directed by: {result.directors}</h4>
+                <p>{result.genres}</p>
+                <p>IMDB rating: {result.imDbRating} |  Metacritic rating: {result.metacriticRating} | Runtime: {result.runtimeStr}</p>
+                <p>{result.plot}</p>
+                
+                </div>
+            </div>
+
+        )
+    })
+
+setPlaying(mappedMovies)
+}
+
+
+useEffect(()=>{
+    displayMovies()
+},[])
+
+    return(
+        <div className="Containerjsh">
+            {playing}
+        </div>
+    )
+}
+
+export default CurrentlyPlaying
