5a46fb8610b42d1417830106d4fb0701f743c98e
divinedouggy <105749979+divinedouggy@users.noreply.github.com>
2022-08-14 03:09:36 -0400

    homepage, logos, css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,9 +7,9 @@ function Home() {
             <img className="buffalo" src={require("../Images/bufflogo.png")} alt="buffalogo" />
             <img className="textlogo" src={require("../Images/textlogo.png")} alt="MOVIE BUFFALO" />
             <div className="pcontainerhome1"><p>We made <span className="yellowtext"> Movie Buffalo </span>for the Movie Buffs out there.</p></div>
-            <div className="pcontainerhome2"><p> See what's at the cinema right now</p></div>
-            <div className="pcontainerhome3"><p> Search through promotional posters for your favorite movies</p></div>
-            <div className="pcontainerhome4"><p> Check if your movie or TV show is available for streaming, and where. </p></div>
+            <div className="pcontainerhome2"><p>See what's at the cinema <span className="yellowtext">right now.</span></p></div>
+            <div className="pcontainerhome3"><p>Search through <span className="yellowtext">promotional posters</span> for your favorite movies.</p></div>
+            <div className="pcontainerhome4"><p>Check if a movie or TV show is <span className="yellowtext">available for streaming,</span> and where. </p></div>
 
 
 
