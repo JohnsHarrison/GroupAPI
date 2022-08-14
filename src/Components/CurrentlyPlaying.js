43fc8e93cd9f8369573d3a4f3d88904d360036e0
divinedouggy <105749979+divinedouggy@users.noreply.github.com>
2022-08-14 16:18:46 -0400

    more css

--- src/Components/CurrentlyPlaying.js
+++ src/Components/CurrentlyPlaying.js
@@ -10,9 +10,10 @@ async function displayMovies(){
     const mappedMovies = movies.map((result,index)=>{
         return(
                 <div className= {index % 2 === 0 ? "MovieContainerjh" : "MovieContainer2jh" } key = {index}>
+               
                 <img className="currentimgjh" src={"https://imdb-api.com/API/ResizeImage?apiKey=k_2zuvks88&size=1012x1500&url=" + result.image} alt=''></img>
                 <div className="Infojh">
-                <h1>{result.title}({result.contentRating})</h1>
+                <h1 className="movietitle">{result.title}({result.contentRating})</h1>
                 <h4>Directed by: {result.directors}</h4>
                 <p>{result.genres}</p>
                 <p>IMDB rating: {result.imDbRating} |  Metacritic rating: {result.metacriticRating} | Runtime: {result.runtimeStr}</p>
